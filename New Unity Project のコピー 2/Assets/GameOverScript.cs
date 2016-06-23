108eb887894388e206890309e7c571718532d2af
hy1107 <hitomiyabe1107@gmail.com>
2016-06-23 23:22:02 +0900

    particle途中

---
+++ New Unity Project のコピー 2/Assets/GameOverScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "Player") {
+			SceneManager.LoadScene ("result");
+		}
+	}
+}
